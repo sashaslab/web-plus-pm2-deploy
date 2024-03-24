fb9a9dd954e9a4463cf1a3bbf5672b08d8934aaa
Alexandr Slobodyanin <sashaslab@yandex.ru>
2024-03-24 17:54:09 +0300

    fix

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'post-deploy': 'npm i && npm run build',
+      'post-deploy': `cd ${DEPLOY_PATH}/current/frontend npm i && npm run build`,
     },
   },
 };
