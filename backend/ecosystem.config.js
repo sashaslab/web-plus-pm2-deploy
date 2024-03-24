2d02a814e17b6157bebb7c3293eb0bd5b5ed7489
Alexandr Slobodyanin <sashaslab@yandex.ru>
2024-03-24 17:24:22 +0300

    fix

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend`,
+      'pre-deploy-local': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend`,
       'post-deploy': 'cd web-plus-pm2-deploy/current/backend && npm i && npm run build',
     },
   },
