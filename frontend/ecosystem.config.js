9b3150c4a3bb0474a14de450a7b097282513d853
Alexandr Slobodyanin <sashaslab@yandex.ru>
2024-03-24 16:40:00 +0300

    add ecosystem.config

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,19 @@
+require('dotenv').config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPO, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+};
