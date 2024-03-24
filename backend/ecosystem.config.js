7df12eac62dfc007ec8bed8a3b7c1d768bda6de0
Alexandr Slobodyanin <sashaslab@yandex.ru>
2024-03-24 17:14:48 +0300

    fix ecosystem

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy': `rsync -av ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'npm i && npm run build',
     },
   },
